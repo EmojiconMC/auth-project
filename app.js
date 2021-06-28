6e110384825a3cbeb11a5641a0dd00113b88beb6
EmojiconMC <andrew@kittymail.com>
2021-06-28 12:58:48 -0400

    Add md5 encryption

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require("dotenv").config()
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const encrypt = require("mongoose-encryption");
+const md5 = require("md5");
 require("ejs");
 
 const app = express();
@@ -17,7 +17,6 @@ const userSchema = new mongoose.Schema({
     email: String, password: String
 });
 
-userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 const User = mongoose.model("User", userSchema);
 
 app.get("/", function(req, res) {
@@ -35,7 +34,7 @@ app.get("/register", function(req, res) {
 app.post("/register", function(req, res) {
     const newUser = new User({
         email: req.body.username,
-        password: req.body.password
+        password: md5(req.body.password)
     });
 
     newUser.save(function(err) {
@@ -49,7 +48,7 @@ app.post("/register", function(req, res) {
 
 app.post("/login", function(req, res) {
     const username = req.body.username;
-    const password = req.body.password;
+    const password = md5(req.body.password);
     User.findOne({email: username}, function(err, user) {
         if (!err) {
             if (user && user.password === password) {
